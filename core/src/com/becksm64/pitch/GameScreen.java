ded3907c117f797dcd051b1c1ea894ead67a3659
Shanebeck4 <becksm64@gmail.com>
2019-05-16 11:26:47 -0400

    Added collection of hands
    Each hand now belongs to a collection of hands, condensed code quite a bit. Refactored things.

--- core/src/com/becksm64/pitch/GameScreen.java
+++ core/src/com/becksm64/pitch/GameScreen.java
@@ -9,6 +9,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameScreen implements Screen {
 
     Game game;
@@ -17,10 +20,7 @@ public class GameScreen implements Screen {
 
     Deck deck;
     CardCollection mainPile;
-    Hand hand;
-    Hand hand2;
-    Hand hand3;
-    Hand hand4;
+    List<Hand> hands;
 
     //Test game logic
     String trump;
@@ -40,10 +40,12 @@ public class GameScreen implements Screen {
         deck.shuffle();
 
         mainPile = new CardCollection();//Create the pile which cards are played to
-        hand = new Hand();//Create test hand
-        hand2 = new Hand();
-        hand3 = new Hand();
-        hand4 = new Hand();
+
+        //Add new hands to collection of hands
+        hands = new ArrayList<Hand>();
+        for(int i = 0; i < 4; i ++) {
+            hands.add(new Hand());
+        }
 
         //Deal hands
         dealHands();
@@ -52,8 +54,6 @@ public class GameScreen implements Screen {
         trump = null;//Starts as null
         currentSuit = null;
         numPlays = 0;//Number of cards played so far in one play
-        //isStartOfRound = true;
-        //isRoundOver = false;
 
         cam = new OrthographicCamera();
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -63,7 +63,7 @@ public class GameScreen implements Screen {
      * Return true if all hands are empty, signifying end of round
      */
     private boolean isRoundOver() {
-        if(hand.isEmpty() && hand2.isEmpty() && hand3.isEmpty() && hand4.isEmpty()) {
+        if(hands.get(0).isEmpty() && hands.get(1).isEmpty() && hands.get(2).isEmpty() && hands.get(3).isEmpty()) {
             return true;
         } return false;
     }
@@ -72,7 +72,7 @@ public class GameScreen implements Screen {
      * Return true if all players have all their cards
      */
     private boolean isStartOfRound() {
-        if(hand.isFull() && hand2.isFull() && hand3.isFull() && hand4.isFull()) {
+        if(hands.get(0).isFull() && hands.get(1).isFull() && hands.get(2).isFull() && hands.get(3).isFull()) {
             return true;
         } else {
             return false;
@@ -84,17 +84,17 @@ public class GameScreen implements Screen {
      */
     private void dealHands() {
         for(int i = 0; i < 6; i++) {
-            hand.addToHand(deck.getTopCard());
-            hand2.addToHand(deck.getTopCard());
-            hand3.addToHand(deck.getTopCard());
-            hand4.addToHand(deck.getTopCard());
+            for(Hand hand : hands) {
+                hand.addToHand(deck.getTopCard());
+            }
         }
     }
 
     /*
-     * Resets deck, player hands, and main card pile for a new round
+     * Resets deck, player hands, trump, and main card pile for a new round
      */
     private void resetRound() {
+        trump = null;
         mainPile.dispose();//I think assets need to be disposed before recreating object
         mainPile = new CardCollection();
         deck.dispose();
@@ -115,51 +115,43 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0.75f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //Update camera
-        cam.update();
+        cam.update();//Update camera
 
         //Update cards in test hands
-        hand.update();
-        hand2.update();
-        hand3.update();
-        hand4.update();
+        for(Hand hand : hands) {
+            hand.update();
+        }
 
         batch.begin();
-        //Test drawing player hand
-        for(int i = 0; i < hand.size(); i++) {
-            Card currentCard = hand.getCard(i);
-            currentCard.setPositionX((Gdx.graphics.getWidth() / 6) * i);
-            batch.draw(currentCard.getCardImage(), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth(), currentCard.getCardHeight());
+        //Draw all hands
+        for(int i = 0; i < hands.size(); i++) {
+
+            Hand currentHand = hands.get(i);
+            for(int j = 0; j < currentHand.size(); j++) {
+
+                Card currentCard = currentHand.getCard(j);
+                if(i == 0) {
+                    currentCard.setPositionX((Gdx.graphics.getWidth() / 6) * j);
+                    batch.draw(currentCard.getCardImage(), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth(), currentCard.getCardHeight());
+                } else if(i == 1) {
+                    currentCard.setPosition((Gdx.graphics.getWidth() / 6) * j, Gdx.graphics.getHeight() - (currentCard.getCardHeight() / 2));
+                    batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 180);
+                } else if(i == 2) {
+                    currentCard.setPosition(Gdx.graphics.getWidth() - (currentCard.getCardWidth() / 2), (currentCard.getCardWidth() * 2) + (Gdx.graphics.getHeight() / 10 * j));
+                    batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 270);
+                } else {
+                    currentCard.setPosition(0 - (currentCard.getCardWidth() / 2), (currentCard.getCardWidth() * 2) + (Gdx.graphics.getHeight() / 10 * j));
+                    batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 270);
+                }
+            }
         }
 
-        //Test drawing computer hand
-        for(int i = 0; i < hand2.size(); i++) {
-            Card currentCard = hand2.getCard(i);
-            currentCard.setPosition((Gdx.graphics.getWidth() / 6) * i, Gdx.graphics.getHeight() - (currentCard.getCardHeight() / 2));
-            batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 180);
-        }
-
-        //Test drawing computer hand 2
-        for(int i = 0; i < hand3.size(); i++) {
-            Card currentCard = hand3.getCard(i);
-            currentCard.setPosition(Gdx.graphics.getWidth() - (currentCard.getCardWidth() / 2), (currentCard.getCardWidth() * 2) + (Gdx.graphics.getHeight() / 10 * i));
-            batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 270);
-        }
-
-        //Test drawing computer hand 3
-        for(int i = 0; i < hand4.size(); i++) {
-            Card currentCard = hand4.getCard(i);
-            currentCard.setPosition(0 - (currentCard.getCardWidth() / 2), (currentCard.getCardWidth() * 2) + (Gdx.graphics.getHeight() / 10 * i));
-            batch.draw(new TextureRegion(currentCard.getCardImage()), currentCard.getPosition().x, currentCard.getPosition().y, currentCard.getCardWidth() / 2, currentCard.getCardHeight() / 2, currentCard.getCardWidth(), currentCard.getCardHeight(), 1, 1, 270);
-        }
-
-        //Test drawing main pile into which cards are played
+        //Draw main pile into which cards are played
         for(int i = 0; i < mainPile.size(); i++) {
             Card currentCard = mainPile.getCard(i);
             currentCard.setPosition((Gdx.graphics.getWidth() / 2) - (currentCard.getCardWidth() / 2), (Gdx.graphics.getHeight() / 2) - (currentCard.getCardHeight() / 2));
             batch.draw(currentCard.getCardImage(), currentCard.getPosition().x + (i * 20), currentCard.getPosition().y + (i * 20), currentCard.getCardWidth(), currentCard.getCardHeight());
         }
-
         batch.end();
 
         //Test playing a card, only if all hands are not empty
@@ -168,80 +160,28 @@ public class GameScreen implements Screen {
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             cam.unproject(touchPos);
 
-            //Check if player card has been touched
-            for(int i = 0; i < hand.size(); i++) {
+            //Loop through each player
+            for(int i = 0; i < hands.size(); i++) {
 
-                Card currentCard = hand.getCard(i);
-                if(currentCard.getBounds().contains(touchPos.x, touchPos.y) && currentCard.isPlayable(trump, currentSuit, numPlays)) {
+                //Check if player card has been touched
+                Hand currentHand = hands.get(i);
+                for(int j = 0; j < currentHand.size(); j++) {
 
-                    if(isStartOfRound()) {
-                        trump = currentCard.getSuit();
+                    Card currentCard = currentHand.getCard(j);
+                    if(currentCard.getBounds().contains(touchPos.x, touchPos.y) && currentCard.isPlayable(trump, currentSuit, numPlays)) {
+
+                        if(isStartOfRound()) {
+                            trump = currentCard.getSuit();
+                        }
+                        System.out.println(trump);
+                        Card cardToPlay = currentHand.playCard(j);
+                        if(numPlays == 0) {
+                            currentSuit = cardToPlay.getSuit();//Test setting currentSuit
+                        }
+                        //trump = cardToPlay.getSuit();//Test setting currentSuit when card is played
+                        mainPile.addToPile(cardToPlay);
+                        numPlays += 1;//Increment the number of cards played so far this play
                     }
-                    System.out.println(trump);
-                    Card cardToPlay = hand.playCard(i);
-                    if(numPlays == 0) {
-                        currentSuit = cardToPlay.getSuit();//Test setting currentSuit
-                    }
-                    //trump = cardToPlay.getSuit();//Test setting currentSuit when card is played
-                    mainPile.addToPile(cardToPlay);
-                    numPlays += 1;//Increment the number of cards played so far this play
-                }
-            }
-
-            //Check if test computer player card has been touched
-            for(int i = 0; i < hand2.size(); i++) {
-
-                Card currentCard = hand2.getCard(i);
-                if(currentCard.getBounds().contains(touchPos.x, touchPos.y) && currentCard.isPlayable(trump, currentSuit, numPlays)) {
-
-                    if(isStartOfRound()) {
-                        trump = currentCard.getSuit();
-                    }
-                    System.out.println(trump);
-                    Card cardToPlay = hand2.playCard(i);
-                    if(numPlays == 0) {
-                        currentSuit = cardToPlay.getSuit();//Test setting currentSuit
-                    }
-                    mainPile.addToPile(cardToPlay);
-                    numPlays += 1;//Increment the number of cards played so far this play
-                }
-            }
-
-            //Check if test computer player 2 card has been touched
-            for(int i = 0; i < hand3.size(); i++) {
-
-                Card currentCard = hand3.getCard(i);
-                if(currentCard.getBounds().contains(touchPos.x, touchPos.y) && currentCard.isPlayable(trump, currentSuit, numPlays)) {
-
-                    if(isStartOfRound()) {
-                        trump = currentCard.getSuit();
-                    }
-                    System.out.println(trump);
-                    Card cardToPlay = hand3.playCard(i);
-                    if(numPlays == 0) {
-                        currentSuit = cardToPlay.getSuit();//Test setting currentSuit
-                    }
-                    mainPile.addToPile(cardToPlay);
-                    numPlays += 1;//Increment the number of cards played so far this play
-                }
-            }
-
-            //Check if test computer player 2 card has been touched
-            for(int i = 0; i < hand4.size(); i++) {
-
-                Card currentCard = hand4.getCard(i);
-                if(currentCard.getBounds().contains(touchPos.x, touchPos.y) && currentCard.isPlayable(trump, currentSuit, numPlays)) {
-
-                    if(isStartOfRound()) {
-                        trump = currentCard.getSuit();
-                    }
-                    System.out.println(trump);
-                    Card cardToPlay = hand4.playCard(i);
-                    if(numPlays == 0) {
-                        currentSuit = cardToPlay.getSuit();//Test setting currentSuit
-                    }
-                    mainPile.addToPile(cardToPlay);
-                    numPlays += 1;//Increment the number of cards played so far this play
                 }
             }
 
@@ -283,10 +223,9 @@ public class GameScreen implements Screen {
 
         batch.dispose();
         deck.dispose();
-        hand.dispose();
-        hand2.dispose();
-        hand3.dispose();
-        hand4.dispose();
+        for(int i = 0; i < hands.size(); i++) {
+            hands.get(i).dispose();
+        }
         mainPile.dispose();
     }
 }
